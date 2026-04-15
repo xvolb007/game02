e39d7f3eea8e82ea3ed4de3e958be028386e6b35
Bohdan Volkov <bohdanvolkovv@gmail.com>
2026-04-15 17:33:43 +0200

    witch

--- game02/Assets/Scripts/PlayerController.cs
+++ game02/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float movingSpeed = 5f;
+    private InputActions inputActions;
+    private Rigidbody2D rigidbody2D;
+    private Vector2 inputVector;
+
+    private void Awake()
+    {
+        inputActions = new InputActions();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
         
     }
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        inputVector = inputActions.Player.Move.ReadValue<Vector2>();
+    }
+    void FixedUpdate()
+    {
+        Vector2 move = inputVector;
+
+        Debug.Log("Input Vector: " + inputVector);
+        Debug.Log($"Normalized Input Vector: " + inputVector.normalized);
+
+
+
+        rigidbody2D.MovePosition(rigidbody2D.position + inputVector * movingSpeed * Time.fixedDeltaTime);
     }
 }
